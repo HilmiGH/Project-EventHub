efa369f94a8fab1e4594ae343031b56a0f475d4e
naufalaschafitz <naufalaschafitz@gmail.com>
2023-06-12 21:55:43 +0700

    bagian nopal pokoknya haha

--- app/Http/Controllers/TopMCController.php
+++ app/Http/Controllers/TopMCController.php
@@ -3,10 +3,166 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TopMCController extends Controller
 {
-    public function TopMC(){
-		return view('TopMC');
-	}
+    public function topMC(Request $request)
+    {
+      $cities = [];
+        if (($handle = fopen(public_path('csv/cities.csv'), 'r')) !== false) {
+            while (($data = fgetcsv($handle, 1000, ',')) !== false) {
+                $parts = explode(',', $data[2]);
+                $city = end($parts);
+                // Extract the words after "KABUPATEN" or "KOTA"
+                preg_match('/(?:KABUPATEN|KOTA)\s(.+)/', $city, $matches);
+                $extractedCity = $matches[1] ?? '';
+
+                $cities[] = $extractedCity;
+
+                // Sort the cities array in ascending order
+                sort($cities);
+            }
+            fclose($handle);
+        }
+
+        //$search = $request->input('search');
+        $akunMCQuery = DB::table('akunMC')
+            ->select('mcID AS id', 'mcUsername AS col1', 'mcFullName AS col2', 'mcCity AS col3', DB::raw("CONCAT('Rp', mcPriceMin, ' -') AS col4"), 'mcPriceMax as col5', DB::raw("'' AS col6"), 'jenisAccountID AS col7', 'ratingMCID AS col8')
+            ->orderByDesc('ratingMCID');
+
+        $topmcresults = $akunMCQuery->paginate(16);
+
+        return view('TopMC', ['akunmc' => $topmcresults, 'akunCounter' => 0, 'cities' => $cities]);
+    }
+
+    public function filter(Request $request)
+    {
+
+        $cities = [];
+        if (($handle = fopen(public_path('csv/cities.csv'), 'r')) !== false) {
+            while (($data = fgetcsv($handle, 1000, ',')) !== false) {
+                $parts = explode(',', $data[2]);
+                $city = end($parts);
+                // Extract the words after "KABUPATEN" or "KOTA"
+                preg_match('/(?:KABUPATEN|KOTA)\s(.+)/', $city, $matches);
+                $extractedCity = $matches[1] ?? '';
+
+                $cities[] = $extractedCity;
+
+                // Sort the cities array in ascending order
+                sort($cities);
+            }
+            fclose($handle);
+        }
+
+        $mcFilter = 0;
+        $eventFilter = 0;
+        $locationFilter = 0;
+        $maxPrice = 0;
+        $minPrice = 0;
+
+        $dateFilter = $request->input('date_filter');
+        $search = $request->input('search');
+        $mcFilter = $request->input('MC');
+        $eventFilter = $request->input('Event');
+        $locationFilter = $request->input('Location');
+        $maxPrice = $request->input('max_price');
+        $minPrice = $request->input('min_price');
+        $eventTypeFilter = $request->input('eventType');
+
+        $akunMCQuery = DB::table('akunMC')
+        ->select('mcID AS id', 'mcUsername AS col1', 'mcFullName AS col2', 'mcCity AS col3', DB::raw("CONCAT('Rp', mcPriceMin, ' -') AS col4"), 'mcPriceMax as col5', DB::raw("'' AS col6"), 'jenisAccountID')
+        ->where(function ($query) use ($search) {
+            $query->where('mcUsername', 'LIKE', "%{$search}%")
+                ->orWhere('mcFullName', 'LIKE', "%{$search}%");
+        });
+
+        if ($mcFilter == "2" && $eventFilter == "3") {
+            $akunMCQuery->where(function ($query) {
+                $query->where('jenisAccountID', '=', "2")
+                    ->orWhere('jenisAccountID', '=', "3");
+            });
+        } elseif ($mcFilter == "2") {
+            $akunMCQuery->where('jenisAccountID', '=', "2");
+        } elseif ($eventFilter == "3") {
+            $akunMCQuery->where('jenisAccountID', '=', "3");
+        }
+
+        if ($locationFilter != "0") {
+            $akunMCQuery->where('mcCity', '=', $locationFilter);
+        }
+
+        if ($maxPrice != "0" && is_numeric($maxPrice)) {
+            $akunMCQuery->where('mcPriceMax', '<=', $maxPrice);
+        }
+
+        if ($minPrice != "0" && is_numeric($minPrice)) {
+            $akunMCQuery->where(function ($query) use ($minPrice, $maxPrice) {
+                $query->where(function ($query) use ($minPrice) {
+                    $query->where('mcPriceMin', '>=', $minPrice)
+                        ->orWhere('mcPriceMax', '>=', $minPrice);
+                });
+
+                if ($maxPrice !== null && is_numeric($maxPrice)) {
+                    $query->where(function ($query) use ($maxPrice) {
+                        $query->where('mcPriceMax', '<=', $maxPrice)
+                            ->orWhere('mcPriceMin', '<=', $maxPrice);
+                    });
+                }
+            });
+        }
+
+
+
+        $eventsQuery = DB::table('events')
+
+        ->select('eventID AS id', 'eventName AS col1', 'numberOfMC AS col2', 'eventLocation AS col3', 'eventDate AS col4', DB::raw("'' AS col5"), 'eventType AS col6', 'jenisAccountID')
+        ->where(function ($query) use ($search) {
+            $query->where('eventName', 'LIKE', "%{$search}%");
+        });
+
+            if ($mcFilter == "2" && $eventFilter == "3") {
+                $eventsQuery->where(function ($query) {
+                    $query->where('jenisAccountID', '=', "2")
+                        ->orWhere('jenisAccountID', '=', "3");
+                });
+            } elseif ($mcFilter == "2") {
+                $eventsQuery->where('jenisAccountID', '=', "2");
+            } elseif ($eventFilter == "3") {
+                $eventsQuery->where('jenisAccountID', '=', "3");
+            }
+            // dd($eventsQuery->get());
+            if ($locationFilter != "0") {
+                $eventsQuery->where('eventLocation', '=', $locationFilter);
+            }
+
+            if ($maxPrice != "0" && is_numeric($maxPrice)) {
+                $eventsQuery->where('jenisAccountID', '=', '2');
+            }
+            if ($minPrice != "0" && is_numeric($minPrice)) {
+                $eventsQuery->where('jenisAccountID', '=', '2');
+            }
+
+            if (!empty($dateFilter)) {
+                $eventsQuery->whereDate('eventDate', '=', $dateFilter);
+                $akunMCQuery->where('jenisAccountID', '=', '3');
+            };
+
+
+            if ($eventTypeFilter) {
+                $eventsQuery->where('eventType', '=', $eventTypeFilter);
+                $akunMCQuery->where('jenisAccountID', '=', '3');
+            };
+
+        $filterResults = $akunMCQuery->union($eventsQuery);
+
+        // $filteredResults = $filterResults->paginate(16);
+        $filteredResults = $filterResults->paginate(16)->appends(request()->except('page'));
+
+        return view('TopMC', ['akunmc' => $filteredResults, 'akunCounter' => 0, 'searchTerm' => $search, 'cities' => $cities]);
+    }
 }
+
+
+
--- resources/views/TopMC.blade.php
+++ resources/views/TopMC.blade.php
@@ -9,171 +9,120 @@
 <!-- isi bagian konten -->
 <!-- cara penulisan isi section yang panjang -->
 @section('content')
+<style>
+    .card:hover {
+        transform: translateY(-5px);
+        border-color: red;
+    }
+    .filterBtn:hover {
+        transform: translateY(-2px);
+    }
+    .truncate-text {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    }
+</style>
     <div class="container" style="padding-right:10rem; min-height: 800px">
         {{-- Filter --}}
-        <div style="position: relative; float: right; height: 10rem; left: 1.5rem">
-            <div style="position: absolute">
-                <div class="filter-container" style="width:215px; background-color: #D9D9D9; border-radius: 38px">
-                    <div class="filter-body" style="padding-top: 10px; padding-left: 20px; padding-right: 20px; padding-bottom: 30px">
-                        <form action="">
-                            <h4 style=" text-align: center;"> Filter</h4>
-                            {{-- <div style="margin-top: 0.75rem; margin-bottom: 0.5rem; font-weight: 600">Category</div>
-                            @foreach ($collection as $item)
-                                <div class="" style="position: relative;
-                                display: block;">
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="mcCheckbox" name="id">
-                                        <label class="custom-control-label" for="mcCheckbox">MC</label>
-                                    </div>
-                                </div>
-                                <div class="" style="position: relative;
-                                display: block;">
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="eventCheckbox" name="event">
-                                        <label class="custom-control-label" for="eventCheckbox">Event</label>
-                                    </div>
-                                </div>
-                            {{-- @endforeach  --}}
-                            <div style="margin-top: 1rem; margin-bottom: 0.5rem; font-weight: 600">Location</div>
-                            {{-- @foreach ($collection as $item)     --}}
-                                <select name="location" class="custom-select">
-                                    <option selected>Select the location</option>
-                                    <option value="Jakarta">Jakarta</option>
-                                    <option value="fiat">Surabaya</option>
-                                    <option value="audi">Semarang</option>
-                                </select>
-                            {{-- @endforeach  --}}
-                            <div style="margin-top: 1rem; margin-bottom: 0.5rem; font-weight: 600">Price</div>
+        <div style="position: absolute; right:3.5rem;">
+            <div class="filter-container" style="width:215px; background-color: #D9D9D9; border-radius: 38px; box-shadow: 2px 2px 2px darkgray;">
+                <div class="filter-body" style="padding-top: 10px; padding-left: 20px; padding-right: 20px; padding-bottom: 30px;">
+                    <form action="{{ route('filter') }}" method="GET">
+                        <!-- Add the search term as a hidden input field -->
+                        <input type="hidden" name="search" value="{{ request('search') }}">
+                        <h4 style=" text-align: center;">
+                            Filter
+                        </h4>
+
+                        <div class="" style="position: relative; display: block;">
+
+                            <div style="margin-top: 1rem; margin-bottom: 0.5rem; font-weight: 600">
+                                Location
+                            </div>
+                            <select name="Location" class="custom-select" style="border-width: 2px; border-color: red; ">
+                                <option value="0" selected>Select the location</option>
+                                @foreach ($cities as $city)
+                                    <option value="{{ $city }}">{{ $city }}</option>
+                                @endforeach
+                            </select>
+                            <div style="margin-top: 1rem; margin-bottom: 0.5rem; font-weight: 600">
+                                Price
+                            </div>
                             <label>Maximum Price</label>
                             <div class="input-group input-group-sm mb-1">
                                 <div class="input-group-prepend">
-                                  <span class="input-group-text" id="filter-maximum-price">Rp</span>
+                                    <span class="input-group-text" id="filter-maximum-price">Rp</span>
                                 </div>
-                                <input type="text" class="form-control" aria-label="maximum price filter" aria-describedby="filter-maximum-price">
+                                <input type="text" class="form-control" name="max_price" aria-label="maximum price filter" aria-describedby="filter-maximum-price">
                             </div>
                             <label>Minimum Price</label>
                             <div class="input-group input-group-sm">
                                 <div class="input-group-prepend">
-                                  <span class="input-group-text" id="filter-minimum-price">Rp</span>
+                                    <span class="input-group-text" id="filter-minimum-price">Rp</span>
                                 </div>
-                                <input type="text" class="form-control" aria-label="minimum price filter" aria-describedby="filter-minimum-price">
+                                <input type="text" class="form-control" name="min_price" aria-label="minimum price filter" aria-describedby="filter-minimum-price">
                             </div>
-                            <div style="margin-top: 1rem; margin-bottom: 0.5rem; font-weight: 600">Date</div>
-                            <div class="form-group">
-                                <input type="date" class="form-control" id="filter-date">
-                              </div>
-                            <div style="margin-top: 1rem; margin-bottom: 0.5rem; font-weight: 600">Event Type</div>
-                            {{-- @foreach ($collection as $item)     --}}
-                                <div class="" style="position: relative;
-                                display: block;">
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="entertainmentCheckbox" name="filter-entertainment">
-                                        <label class="custom-control-label" for="entertainmentCheckbox">Entertainment</label>
-                                    </div>
-                                </div>
-                                <div class="" style="position: relative;
-                                display: block;">
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="educationCheckbox" name="filter-education">
-                                        <label class="custom-control-label" for="educationCheckbox">Education</label>
-                                    </div>
-                                </div>
-                            {{-- @endforeach  --}}
-                            <button type="submit" class="btn btn-danger mt-3">Filter</button>
-                        </form>
-                    </div>
+
+                            <button type="submit" class="btn btn-danger mt-3 filterBtn">
+                                Filter
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
         <div class="row"  style="margin-top:110px; margin-bottom:30px">
-            <div class="col-md">
-                <div class="card" style="width:200px; margin-bottom: 2rem">
-                    <img class="card-img-top" src="profile.JPG" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">John Doe</h4>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and
-                            engineer</p>
-                        <a href="#" class="btn btn-danger">See Profile</a>
+            @foreach ($akunmc as $info_akun)
+            @if ($akunCounter>=20)
+                @break
+            @endif
+            <div class="col-md-3" style="margin-bottom: 3rem;">
+                <div class="card" style="width:200px; min-height: 24rem; border-radius: 15px">
+                    <img class="card-img-top" style="border-top-right-radius: 15px; border-top-left-radius: 15px" src= {{ asset('img/Portrait.png') }} alt="Card image" style="width:100%;">
+                    <div class="card-body" style="display: grid;">
+                        <h4 class="card-title truncate-text" style="text-align: center;">{{ $info_akun->col1 }}</h4>
+                        <p class="card-text" style="text-align: center;">{{ $info_akun->col3 }}</p>
+                        <p class="card-text" style="text-align: center;">{{ $info_akun->col4 .' '. $info_akun ->col5}}</p>
+                        {{-- <h4 class="card-title">{{ $info_akun->mcUsername }}</h4>
+                        <p class="card-text">{{ $info_akun->mcCity }}</p>
+                        <p class="card-text">Rp {{ $info_akun->mcPriceMin .' - '. $info_akun ->mcPriceMax}}</p> --}}
+                        <a href="#" class="btn btn-danger" style="border-radius: 15px"> {{ $info_akun->col6 }} </a>
                     </div>
                 </div>
             </div>
-            <div class="col-md">
-                <div class="card" style="width:200px; margin-bottom: 2rem">
-                    <img class="card-img-top" src="profile.JPG" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">John Doe</h4>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and
-                            engineer</p>
-                        <a href="#" class="btn btn-danger">See Profile</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md">
-                <div class="card" style="width:200px; margin-bottom: 2rem">
-                    <img class="card-img-top" src="profile.JPG" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">John Doe</h4>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and
-                            engineer</p>
-                        <a href="#" class="btn btn-danger">See Profile</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md">
-                <div class="card" style="width:200px; margin-bottom: 2rem">
-                    <img class="card-img-top" src="profile.JPG" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">John Doe</h4>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and
-                            engineer</p>
-                        <a href="#" class="btn btn-danger">See Profile</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md">
-                <div class="card" style="width:200px; margin-bottom: 2rem">
-                    <img class="card-img-top" src="profile.JPG" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">John Doe</h4>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and
-                            engineer</p>
-                        <a href="#" class="btn btn-danger">See Profile</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md">
-                <div class="card" style="width:200px; margin-bottom: 2rem">
-                    <img class="card-img-top" src="profile.JPG" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">John Doe</h4>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and
-                            engineer</p>
-                        <a href="#" class="btn btn-danger">See Profile</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md">
-                <div class="card" style="width:200px; margin-bottom: 2rem">
-                    <img class="card-img-top" src="profile.JPG" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">John Doe</h4>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and
-                            engineer</p>
-                        <a href="#" class="btn btn-danger">See Profile</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md">
-                <div class="card" style="width:200px; margin-bottom: 2rem">
-                    <img class="card-img-top" src="profile.JPG" alt="Card image" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">John Doe</h4>
-                        <p class="card-text">Some example text some example text. John Doe is an architect and
-                            engineer</p>
-                        <a href="#" class="btn btn-danger">See Profile</a>
-                    </div>
-                </div>
-            </div>
+            @php
+                $akunCounter++;
+            @endphp
+            @endforeach
         </div>
     </div>
+    </div>
+    <div class="" style="display: flex; justify-content: center; align-content: center">
+        <div class="" style="margin-right: 10rem; margin-bottom: 2rem">
+            {{ $akunmc->appends(request()->except('page'))->links() }}
+        </div>
+    </div>
+</div>
+<script>
+    // Truncate the text when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+  var cardTitles = document.querySelectorAll('.card-title');
+  cardTitles.forEach(function(title) {
+    truncateText(title, 3);
+  });
+});
+
+// Truncate the text based on the specified number of lines
+function truncateText(element, lines) {
+  var lineHeight = parseInt(getComputedStyle(element).lineHeight, 10);
+  var maxHeight = lineHeight * lines;
+
+  if (element.offsetHeight > maxHeight) {
+    while (element.offsetHeight > maxHeight) {
+      element.textContent = element.textContent.replace(/\W*\s(\S)*$/, '...');
+    }
+  }
+}
+</script>
 @endsection
