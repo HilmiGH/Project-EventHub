03f73a7515f375756c03e0451d99785374be2b8a
IvanBarlianto <ivanasshidiqqi@gmail.com>
2023-06-13 15:11:06 +0700

    rating fix

--- resources/views/DetailedInfo.blade.php
+++ resources/views/DetailedInfo.blade.php
@@ -9,12 +9,11 @@
         <div class="col-md-6 offset-md-3">
             @foreach ($akunmc as $info_akun)
             <p>Username: {{ $info_akun->mcUsername }}</p>
-            <p>Full Name: {{ $info_akun->mcFullName }}</p>
             <p>Phone: {{ $info_akun->mcPhone }}</p>
             <p>City: {{ $info_akun->mcCity }}</p>
-            <p>Language: {{ $info_akun->mcLanguage }}</p>
             <p>Specialization: {{ $info_akun->mcSpecialization }}</p>
             <p>Experience: {{ $info_akun->mcExperience }}</p>
+            <p>Rating MC: {{ $info_akun->ratingMCID }}</p>
             <p>Price Range: {{ $info_akun->mcPriceMin }} - {{ $info_akun->mcPriceMax }}</p>
             @endforeach
         </div>
