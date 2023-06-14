14e3ae4b825fd3e106e56ab918f0a6826b246e89
IvanBarlianto <ivanasshidiqqi@gmail.com>
2023-06-14 23:03:51 +0700

    oke

--- resources/views/DetailedInfo.blade.php
+++ resources/views/DetailedInfo.blade.php
@@ -8,13 +8,13 @@
     <div class="row" style="margin-top: 2.5cm">
         <div class="col-md-6 offset-md-3">
             @foreach ($akunmc as $info_akun)
-            <p>Username: {{ $info_akun->mcUsername }}</p>
-            <p>Phone: {{ $info_akun->mcPhone }}</p>
-            <p>City: {{ $info_akun->mcCity }}</p>
-            <p>Specialization: {{ $info_akun->mcSpecialization }}</p>
-            <p>Experience: {{ $info_akun->mcExperience }}</p>
-            <p>Rating MC: {{ $info_akun->ratingMCID }}</p>
-            <p>Price Range: {{ $info_akun->mcPriceMin }} - {{ $info_akun->mcPriceMax }}</p>
+            <p>Username : {{ $info_akun->mcUsername }}</p>
+            <p>Phone : {{ $info_akun->mcPhone }}</p>
+            <p>City : {{ $info_akun->mcCity }}</p>
+            <p>Specialization : {{ $info_akun->mcSpecialization }}</p>
+            <p>Experience : {{ $info_akun->mcExperience }}</p>
+            <p>Rating MC : {{ $info_akun->ratingMCID }}</p>
+            <p>Price Range : Rp. {{ $info_akun->mcPriceMin }} - Rp. {{ $info_akun->mcPriceMax }}</p>
             @endforeach
         </div>
     </div>
