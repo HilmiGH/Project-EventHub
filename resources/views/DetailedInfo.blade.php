a61a9f04a993d9730c661fa3cdd74df5970487d3
afiff15 <afiframadhan1511@gmail.com>
2023-06-15 16:12:09 +0700

    finalisasi detailed info dan review

--- resources/views/DetailedInfo.blade.php
+++ resources/views/DetailedInfo.blade.php
@@ -3,21 +3,94 @@
 @section('judul_halaman', 'Detail Profile')
 
 @section('content')
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
 
-<div class="container">
-    <div class="row" style="margin-top: 2.5cm">
-        <div class="col-md-6 offset-md-3">
+
+    <section style="background-color: #eee;">
+        <div class="container py-5" style="margin-top: 1.5cm">
             @foreach ($akunmc as $info_akun)
-            <p>Username: {{ $info_akun->mcUsername }}</p>
-            <p>Phone: {{ $info_akun->mcPhone }}</p>
-            <p>City: {{ $info_akun->mcCity }}</p>
-            <p>Specialization: {{ $info_akun->mcSpecialization }}</p>
-            <p>Experience: {{ $info_akun->mcExperience }}</p>
-            <p>Rating MC: {{ $info_akun->ratingMCID }}</p>
-            <p>Price Range: {{ $info_akun->mcPriceMin }} - {{ $info_akun->mcPriceMax }}</p>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="card mb-4">
+                            <div class="card-body text-center">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                                    alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+                                <h5 class="my-3">{{ $info_akun->mcUsername }}</h5>
+                                <p class="text-muted mb-1" style="margin-top: -0.3cm">{{ $info_akun->mcCity }}</p>
+                                <p class="text-muted mb-4">Rating: {{ $info_akun->ratingMCID }} Stars</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <p class="mb-0">Username</p>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <p class="text-muted mb-0">{{ $info_akun->mcUsername }}</p>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <p class="mb-0">City</p>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <p class="text-muted mb-0">{{ $info_akun->mcCity }}</p>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <p class="mb-0">Specialization</p>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <p class="text-muted mb-0">{{ $info_akun->mcSpecialization }}</p>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <p class="mb-0">Experience</p>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <p class="text-muted mb-0">{{ $info_akun->mcExperience }}</p>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <p class="mb-0">Price Range</p>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <p class="text-muted mb-0">{{ $info_akun->mcPriceMin }} -
+                                            {{ $info_akun->mcPriceMax }}</p>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <p class="mb-0">Phone Number</p>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <p class="text-muted mb-0">{{ $info_akun->mcPhone }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <h4 style="margin-right: 0.3cm; margin-left: 0.5cm">Reviews</h4>
+                            <button><a href="/landingpage/morerating" class='fas fa-angle-right'></a></button>
+                        </div>
+                    </div>
+                </div>
             @endforeach
         </div>
-    </div>
-</div>
+    </section>
 
 @endsection
